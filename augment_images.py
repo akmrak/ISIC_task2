bc71e927b511598fe5391cb12ea39ba5e3a0cbf8
Abhishek Kumar <abhishek.kmr.ak@gmail.com>
2018-07-01 06:50:30 -0400

    tiled image count modified

--- augment_images.py
+++ augment_images.py
@@ -12,6 +12,8 @@ count_base_no=0
 base_image_ht=224
 base_image_wd=224
 stride_length=10
+count_1=[0,0,0,0,0]
+count_0=[0,0,0,0,0]
 def check_matrix(image,mask):
 	median_image=np.median(mask)
 	#print(mask.shape)
@@ -42,8 +44,12 @@ def generate_augmented_images(names,attribute):
 		while(i+base_image_wd<wd):
 			#print(i,i+base_image_wd,j,j+base_image_ht)
 			#print(original_image[i:i+base_image_wd,j:j+base_image_ht,:].shape,image_mask[i:i+base_image_wd,j:j+base_image_ht].shape)
-			print(i,i+base_image_wd,j,j+base_image_ht)
-			check=check_matrix(original_image[i:i+base_image_wd,j:j+base_image_ht,:],image_mask[i:i+base_image_wd,j:j+base_image_ht])
+			#print(i,i+base_image_wd,j,j+base_image_ht)
+			i1=i+base_image_wd
+			j1=j+base_image_ht
+			check=check_matrix(original_image[j:j1,i:i1,:],image_mask[j:j1,i:i1])
+			#print(i,i1,j,j1)
+			#print(original_image[j:j1,i:i1,:].shape,image_mask[j:j1,i:i1].shape)
 			#print(check)	
 			if(check==1):
 				val_1+=1
@@ -51,17 +57,19 @@ def generate_augmented_images(names,attribute):
 				val_0+=1
 			i+=stride_length
 		j+=stride_length
-	print(val_0,val_1)	
+	#print(val_0,val_1)	
+	count_0[attribute-1]+=val_0
+	count_1[attribute-1]+=val_1
 def generate_driver(names):
 	#attribute_count=1
 	for attribute_num in range(attribute_count):
 		for name in names:
-			#print(name)
 			generate_augmented_images(name,attribute_num+1)
 def main():
 	list_names=pickle.load(open(string_names,"rb"))
-	list_names=list_names[:1]
+	#list_names=list_names[:2]
 	generate_driver(list_names)	
+	print(count_0,count_1)
 if __name__ == "__main__":
 	main()
 
